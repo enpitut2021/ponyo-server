4f87c37ed94acf08bd709ad3a4ffec79d1df4dde
notch_man <30828280+notchman8600@users.noreply.github.com>
2021-10-23 15:44:44 +0900

    Postgresの接続用オブジェクトを作成するメソッドを追加

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,11 +1,27 @@
 # coding: utf-8
 from flask import Flask, request, render_template, redirect, jsonify, make_response
-
+import os
+import psycopg2
+import urllib.parse
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
+
+# バッドコードな気がしますが...
+
+def getConn():
+    urllib.parse.uses_netloc.append("postgres")
+    url = urllib.parse.urlparse(os.environ["SQL_URL"])
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    return conn
 
 
 @app.route("/dest", methods=['GET'])
@@ -19,10 +35,9 @@ def get_sample():
     }), 200)
 
 
-@app.route("/post_spot", methods=['POST'])
+@app.route("/task", methods=['POST'])
 def new_spot():
-    # jsonオブジェクトを取得
-    # json_obj = request.json
+    request = request.json
 
     # ここでスポット登録
 
